19ee7c1821b03d354dadac4510f4fe1a74711401
mingsound <62479120+mingsound21@users.noreply.github.com>
2022-12-30 01:17:52 +0900

    [#6] Feat: 로그인 한 경우 userName 화면에 전달

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.dto.SessionUser;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -9,15 +10,24 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){// 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
         model.addAttribute("posts", postsService.findAllDesc());// postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
+
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
