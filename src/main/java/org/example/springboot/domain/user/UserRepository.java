584ca1514df5c760a0181f6e1019660c94c91465
mingsound <62479120+mingsound21@users.noreply.github.com>
2022-12-30 01:16:27 +0900

    [#6] Feat: User Entity, Repository, Role Enum 생성

--- src/main/java/org/example/springboot/domain/user/UserRepository.java
+++ src/main/java/org/example/springboot/domain/user/UserRepository.java
@@ -1,2 +1,9 @@
-package org.example.springboot.domain.user;public interface UserRepository {
+package org.example.springboot.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);// email을 통해서 이미 생성된 사용자인지, 처음 가입하는 사용자인지 판단
 }
